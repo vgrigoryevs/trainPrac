2cea82bfb50461c7bfa4b08f62b96417b7fc2807
Vladimir Grigoryev <vgrigoryev@smartru.com>
2015-03-04 15:09:07 +0300

    added a NaN check for input

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+function makeNewWagon() {
+    var name = document.getElementById("nameOfWagon").value,
+        e = document.getElementById("newWagon"),
+        wagonClass = e.options[e.selectedIndex].value,
+        bottomSeat = document.getElementById("bottomSeat").value,
+        topSeat = document.getElementById("topSeat").value,
+        bottomSideSeat = document.getElementById("bottomSideSeat").value,
+        topSideSeat = document.getElementById("topSideSeat").value;
+
+    if(isNaN(+bottomSeat)||isNaN(+topSeat)||isNaN(+bottomSideSeat)||isNaN(+topSideSeat)) {
+        return alert("Неправильный ввод данных");
+    }
+
+    if(wagonClass === "CompartmentWagon") {
+        window.wagons[name] = new CompartmentWagon(bottomSeat, topSeat, bottomSideSeat, topSideSeat);
+    }
+    else if(wagonClass === "ReservedWagon") {
+        window.wagons[name] = new ReservedWagon(bottomSeat, topSeat, bottomSideSeat, topSideSeat);
+    }
+    else {
+        window.wagons[name] = new Sw(bottomSeat, topSeat, bottomSideSeat, topSideSeat);     
+    }
+
+    train.addWagon(window.wagons[name]);
+    addWagonToHtml(window.wagons[name]);
+
+    alert("Доходность вагона " + window.wagons[name].wagonIncome());
+}
+
+function choseOption() {
+    var wagonClass = document.getElementById("newWagon").value;
+    if(wagonClass === "ReservedWagon") {
+        document.getElementById("bottomSideSeatText").style.display = "none";
+        document.getElementById("bottomSideSeat").style.display = "none";
+        document.getElementById("topSideSeat").style.display = "none";
+        document.getElementById("topSideSeatText").style.display = "none";
+        document.getElementById("topSeat").style.display = "block"
+        document.getElementById("topSeatText").style.display = "block"
+    }
+    else if(wagonClass === "CompartmentWagon") {
+        document.getElementById("bottomSideSeatText").style.display = "block";
+        document.getElementById("bottomSideSeat").style.display = "block";
+        document.getElementById("topSideSeat").style.display = "block";
+        document.getElementById("topSideSeatText").style.display = "block";
+        document.getElementById("topSeat").style.display = "block"
+        document.getElementById("topSeatText").style.display = "block"
+    }
+    else {
+        document.getElementById("bottomSideSeatText").style.display = "none";
+        document.getElementById("bottomSideSeat").style.display = "none";
+        document.getElementById("topSideSeat").style.display = "none";
+        document.getElementById("topSideSeatText").style.display = "none";
+        document.getElementById("topSeat").style.display = "none"
+        document.getElementById("topSeatText").style.display = "none"
+    }
+}
+
+function addWagonToHtml(wagonName) {
+    var table = document.getElementById("allWagons"),
+        tr = table.insertRow(-1),
+        name = tr.insertCell(0),
+        type = tr.insertCell(1),
+        bottomSeat = tr.insertCell(2),
+        topSeat = tr.insertCell(3),
+        bottomSideSeat = tr.insertCell(4),
+        topSideSeat = tr.insertCell(5),
+        income = tr.insertCell(6),
+        e = document.getElementById("newWagon"),
+        wagonClass = e.options[e.selectedIndex].innerHTML;
+        name.innerHTML = document.getElementById("nameOfWagon").value;
+        bottomSeat.innerHTML = wagonName.bottomSeat;
+        topSeat.innerHTML = wagonName.topSeat;
+        bottomSideSeat.innerHTML = wagonName.bottomSideSeat;
+        topSideSeat.innerHTML = wagonName.topSideSeat;
+        type.innerHTML = wagonClass;
+        income.innerHTML = wagonName.wagonIncome();
+}
+
 function TrainWagon(bottomSeat, topSeat, bottomSideSeat, topSideSeat, options) {
     'use strict';
     this.bottomSeat = bottomSeat;
@@ -80,10 +158,5 @@ PassengerTrain.prototype.trainIncome = function trainIncome() {
 
 
 var train = new PassengerTrain(),
-    firstWagon = new CompartmentWagon(10,10,10,10),
-    secondWagon = new ReservedWagon(10,10,10,10),
-    thirdWagon = new Sw(10,10,10,10);
-    
-train.addWagon(firstWagon);
-train.addWagon(secondWagon);
-train.addWagon(thirdWagon);
+    wagons = {};
+
